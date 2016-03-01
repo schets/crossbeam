3dd6d3dc271f439f49a2fbcad79a56d9fbfc5a57
Sam Schetterer <schets@users.noreply.github.com>
2016-03-01 14:09:43 -0500

    single writer hashtab;e

---
+++ src/sync/sw_hashtable.rs
@@ -0,0 +1,17 @@
+use std::sync::atomic::Ordering::{Acquire, Release, Relaxed};
+use std::sync::atomic::AtomicBool;
+use std::{ptr, mem};
+use std::thread::{self, Thread};
+
+use mem::epoch::{self, Atomic, Owned, Shared};
+
+struct Node<K, V> {
+    key: K,
+    val: V,
+    hash: u64,
+    Atomic<Node<K, V>> next,
+}
+
+struct Table<K, V> {
+
+}
