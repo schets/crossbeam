9ec9ce04002e5447f94eb342d5298c413de8b5c0
Sam Schetterer <schets@users.noreply.github.com>
2016-03-01 17:39:37 -0500

    saving

--- src/sync/sw_trie.rs
+++ src/sync/sw_trie.rs
@@ -5,14 +5,29 @@ use std::thread::{self, Thread};
 
 use mem::epoch::{self, Atomic, Owned, Shared};
 
-struct Node<K, V> {
+static ARRAY_SIZE: i32 = 32;
+type Bitarray = i32;
+
+enum NodeType<K, V> {
+    Atomic<ElemNode<K, V>>,
+    Atomic<ArrayNode<K, V>>,
+}
+
+struct ElemNode<K, V> {
+    hash: u64,
     key: K,
     val: V,
-    hash: u64,
-    Atomic<Node<K, V>> next_iter,
-    Atomic<Node<K, V>>
+}
+
+struct ArrayNode<K, V> {
+    bits: Bitarray,
+    ptrs: [NodeType, ARRAY_SIZE];
 }
 
 struct Table<K, V> {
+    Atomic<ArrayNode<K, V>> root;
+}
+
+impl<K, V> Table<K, V> {
 
 }
