dd06220d20780f5bc749ab0e942aa74cfc0a469a
Sam Schetterer <schets@users.noreply.github.com>
2016-03-02 16:08:12 -0500

    Saving for the time being

--- src/sync/sw_trie.rs
+++ src/sync/sw_trie.rs
@@ -7,7 +7,9 @@ use mem::epoch::{self, Atomic, Owned, Shared, Guard};
 
 const ARRAY_SIZE: usize = 0b100000; // 32
 const IND_MASK: u64 = 0b011111; // 31
-const SHIFT_BITS: u32 = 5; /// number of bits used in local array index
+const SHIFT_BITS: u32 = 5; // number of bits used in local array index
+const MAX_DEPTH: usize = 12; // Once we are in the 12th level the hash is exhausted
+
 
 type Bitarray = i32;
 
@@ -20,48 +22,44 @@ fn lower_hash(h: u64) -> u64 { h >> SHIFT_BITS }
 #[inline(always)]
 fn get_active(b: Bitarray, i: usize) -> bool { (b & (1 << i)) != 0 }
 
-enum NodeType<K, V> {
-    Elem(Atomic<ElemNode<K, V>>),
-    Array(Atomic<ArrayNode<K, V>>),
-    Null,
+enum Node<K, V> {
+    Elem{key: K, val: V, h: u64, next: Atomic<Elem<K, V>>},
+    Array{bits: Bitarray, ptrs: [Atomic<Node<K, V>>; ARRAY_SIZE]},
 }
 
-struct ElemNode<K, V> {
-    next: Atomic<ElemNode<K, V>>,
-    hash: u64,
-    key: K,
-    val: V,
-}
-
-struct ArrayNode<K, V> {
-    bits: Bitarray,
-    ptrs: [NodeType<K, V>; ARRAY_SIZE],
-}
-
-impl<K, V> ArrayNode<K, V> {
-    pub fn new() -> ArrayNode<K, V> {
-        let mut a = ArrayNode {
-            bits: 0,
-            ptrs: unsafe { mem::uninitialized() },
-        };
-        for i in 0..ARRAY_SIZE {
-            a.ptrs[i] = NodeType::Elem(Atomic::null());
-        }
-        a
+fn new_ar<K, V>() -> Node<K, V> {
+    let mut a = Node::Array {
+        bits: 0,
+        ptrs: unsafe { mem::uninitialized() },
+    };
+    for i in 0..ARRAY_SIZE {
+        a.ptrs[i] = NodeType::Elem(Atomic::null());
     }
+    a
+}
 
-    pub fn node_lookup(&self, h: u64, g: &Guard) -> bool {
-        let ind = get_index(h);
-        if get_active(self.bits, ind) {
-            match self.ptrs[ind] {
-                NodeType::Elem(ref ptr) => true,
-                NodeType::Array(ref ptr) => {
-                    ptr.load(Acquire, g).unwrap().node_lookup(lower_hash(h), g)
-                },
-                NodeType::Null => false
-            }
+fn node_lookup(node: &Node<K, V>, h: u64, g: &Guard) -> bool {
+    let ind = get_index(h);
+    if get_active(node.bits, ind) {
+        match node.ptrs[ind] {
+            NodeType::Elem(ref ptr) => true,
+            NodeType::Array(ref ptr) => {
+                node_lookup(&ptr.load(Acquire, g).unwrap(), lower_hash(h), g)
+            },
+            NodeType::Null => false
         }
-        else { false }
+    }
+    else { false }
+}
+
+fn insert_node<K, V>(node: &mut Node::Array<K, V>, h: u64, k: K, v: V, g: &Guard) {
+    let ind = get_index(h);
+    if get_active(node.bites, ind) {
+
+    }
+    else {
+        let o = Owned::new(Node::Elem{key: k, val: v, h: h, });
+        node.ptrs[ind].store(o, Release, g);
     }
 }
 
