f43d37c5121bd49b0b85b301a9fe42de1e9d42df
Sam Schetterer <schets@users.noreply.github.com>
2016-03-02 14:50:22 -0500

    Some work in progress

--- src/sync/sw_trie.rs
+++ src/sync/sw_trie.rs
@@ -3,14 +3,27 @@ use std::sync::atomic::AtomicBool;
 use std::{ptr, mem};
 use std::thread::{self, Thread};
 
-use mem::epoch::{self, Atomic, Owned, Shared};
+use mem::epoch::{self, Atomic, Owned, Shared, Guard};
 
-const ARRAY_SIZE: usize = 32;
+const ARRAY_SIZE: usize = 0b100000; // 32
+const IND_MASK: u64 = 0b011111; // 31
+const SHIFT_BITS: u32 = 5; /// number of bits used in local array index
+
 type Bitarray = i32;
+
+#[inline(always)]
+fn get_index(h: u64) -> usize { (h & IND_MASK) as usize }
+
+#[inline(always)]
+fn lower_hash(h: u64) -> u64 { h >> SHIFT_BITS }
+
+#[inline(always)]
+fn get_active(b: Bitarray, i: usize) -> bool { (b & (1 << i)) != 0 }
 
 enum NodeType<K, V> {
     Elem(Atomic<ElemNode<K, V>>),
     Array(Atomic<ArrayNode<K, V>>),
+    Null,
 }
 
 struct ElemNode<K, V> {
@@ -36,6 +49,20 @@ impl<K, V> ArrayNode<K, V> {
         }
         a
     }
+
+    pub fn node_lookup(&self, h: u64, g: &Guard) -> bool {
+        let ind = get_index(h);
+        if get_active(self.bits, ind) {
+            match self.ptrs[ind] {
+                NodeType::Elem(ref ptr) => true,
+                NodeType::Array(ref ptr) => {
+                    ptr.load(Acquire, g).unwrap().node_lookup(lower_hash(h), g)
+                },
+                NodeType::Null => false
+            }
+        }
+        else { false }
+    }
 }
 
 struct Table<K, V> {
@@ -48,4 +75,6 @@ impl<K, V> Table<K, V> {
             root: Atomic::new(ArrayNode::new())
         }
     }
+
+
 }
