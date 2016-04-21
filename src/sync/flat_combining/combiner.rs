f2e7b056f595a4353f6e76de06c754d0c589cbc8
Sam Schetterer <schets@users.noreply.github.com>
2016-04-21 13:56:52 -0400

    Getting there

--- src/sync/flat_combining/combiner.rs
+++ src/sync/flat_combining/combiner.rs
@@ -1,14 +1,16 @@
 use std::sync::atomic::{AtomicPtr, AtomicBool, AtomicUsize, fence};
 use std::sync::atomic::Ordering::{Relaxed, Acquire, Release};
-use std::cell::UnsafeCell;
+use std::sync::{Mutex, Condvar};
+use std::cell::Cell;
 use std::thread;
 use std::mem;
 use std::ptr;
 
 const WAITING: usize = 0;
 const IN_PROGRESS: usize = 1;
-const COMPLETED: usize = 2;
-const POISONED: usize = 3;
+const TAKE_OVER = 2;
+const COMPLETED: usize = 3;
+const POISONED: usize = 4;
 
 struct Message {
     op: unsafe fn(),
@@ -30,10 +32,16 @@ impl Message {
 
         unsafe { (self.op)() };
         self.status.store(COMPLETED, Release);
-        self.awaken();
+
+        // Prevent reordering of on_completed after completion?
+        // Can't do just on client side, but would really, really like to not
+        // have to
+        self.on_completed();
     }
 
     fn awaken(&self) {}
+
+    fn on_completed(&self) {}
 
     pub fn propagate_panic(&self) {}
 }
@@ -50,7 +58,14 @@ pub struct FlatCombiner {
     used: AtomicBool,
 
     poisoned: AtomicBool,
+
+    // Could really just use a cheaper mechanism
+    wakeup: Condvar,
+
+    wakeup_mut: Mutex<bool>,
 }
+ unsafe impl Send for FlatCombiner {}
+ unsafe impl Sync for FlatCombiner {}
 
 impl FlatCombiner {
     pub fn new() -> FlatCombiner {
@@ -59,62 +74,146 @@ impl FlatCombiner {
             local_messages: Cell::new(ptr::null_mut()),
             used: AtomicBool::new(false),
             poisoned: AtomicBool::new(false),
+            wakeup: Condvar::new(),
+            wakeup_mut: Mutex::new(false),
         }
     }
 
-    fn load_messages(&self) -> bool {
+    fn load_messages(&self) -> Option<*mut Message> {
         // Make the local_messages check an invariant?
         if self.local_messages.get() == ptr::null_mut() &&
            self.message_stack_head.load(Relaxed) != ptr::null_mut() {
             let head = self.message_stack_head.swap(ptr::null_mut(), Acquire);
             self.local_messages.set(head);
+            Some(head)
+        }
+        else { None }
+    }
+
+    fn get_a_message(&self) -> Option<*mut Message> {
+        let mut mhead = self.local_messages.get();
+        if mhead == ptr::null_mut() {
+            match self.load_messages() {
+                None => return None,
+                Some(head) => mhead = head,
+            };
+        }
+        Some(mhead)
+    }
+
+    fn get_and_process(&self) -> bool {
+        if let Some(message) = self.get_a_message() {
+            unsafe { (&*message).process() };
             true
         }
         else { false }
     }
 
-    fn get_and_process(&self) -> usize {
-        0
-    }
-
     // No panic handling yet...
     fn read_messages(&self, n_max: usize) {
-        for i in 0..n_max {
+        for _ in 0..n_max {
             if !self.get_and_process() { break; }
         }
     }
 
     fn alert_next(&self) {
+        self.wakeup.notify_all();
     }
 
     fn poison_self(&self) {}
 
-    fn try_operation<F: Send + FnOnce()>(&self, op: *const F) -> bool {
+    #[inline(always)]
+    fn try_operation<F: Send + FnOnce()>(&self, op: F) -> Option<F> {
         let lock_val = self.used.load(Relaxed);
-        if lock_val || !self.used.compare_and_swap(false, true, Acquire) {
-            false
+        if lock_val || self.used.compare_and_swap(false, true, Acquire) {
+            Some(op)
         }
         else {
+            op();
             self.read_messages(20);
-            true
+            self.alert_next();
+            None
         }
     }
 
-    pub fn submit<F: Send + FnOnce()>(&self, op: F) {
-        if !self.try_operation(&op) {
-            let mut message = Message::new(unsafe { mem::transmute(&op) });
-            loop {
-                let cur_head = self.message_stack_head.load(Relaxed);
-                message.next.store(cur_head, Relaxed);
-                let old_head = self.message_stack_head.compare_and_swap(cur_head,
-                                                                        &mut message,
-                                                                        Release);
-                if old_head == cur_head {
+    pub fn submit<F: Send + FnOnce() -> R, R: Send>(&self, _op: F) -> R {
+        let mut rval: R = unsafe { mem::uninitialized() };
+        {
+            let rval_ref = &mut rval;
+            let op = || *rval_ref = _op();
+            if let Some(op) = self.try_operation(op) {
+                let mut message = Message::new(unsafe { mem::transmute(&op) });
+                loop {
+                    let cur_head = self.message_stack_head.load(Relaxed);
+                    message.next.store(cur_head, Relaxed);
+                    let old_head = self.message_stack_head.compare_and_swap(cur_head,
+                        &mut message,
+                        Release);
+                    if old_head == cur_head {
+                        break;
+                    }
+                }
+
+                // ewwwwwwwww
+                // Move into dedicated function,  will satisfy owner shenanigans
+                'goto: loop {
+                    for _ in 0..50 {
+                        if message.status.load(Relaxed) == COMPLETED {
+                            break 'goto;
+                        }
+                    }
+
+                    loop {
+                        if message.status.load(Relaxed) == COMPLETED {
+                            break 'goto;
+                        }
+                        thread::yield_now();
+                    }
+
+                    // fat, heavy waiting loop. Hopefully this is never reached
+                    // Also, future schemes will let users specify the impl...
+                    let mut waiting = self.wakeup_mut.lock().unwrap();
+                    while message.status.load(Relaxed) != COMPLETED {
+                        waiting = self.wakeup.wait(waiting).unwrap();
+                    }
                     break;
                 }
+                fence(Acquire);
             }
-            while message.status.load(Relaxed) != COMPLETED { thread::yield_now(); }
-            fence(Acquire)
         }
+        rval
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use scope;
+    use super::*;
+
+    #[test]
+    pub fn test_basic() {
+        let comb = FlatCombiner::new();
+        let mut x = 0;
+
+        let rval = comb.submit(|| { x += 1; x+1});
+        assert_eq!(rval, 2);
+        assert_eq!(x, 1);
+    }
+
+    #[test]
+    pub fn test_thread() {
+        let comb = FlatCombiner::new();
+        scope(|scope| {
+            scope.spawn( || {
+                for _ in 0..1000 {
+                    let mut x = 0;
+
+                    let rval = comb.submit(|| { x += 1; x+1});
+                    assert_eq!(rval, 2);
+                    assert_eq!(x, 1);
+                }
+            })
+        });
     }
 }
